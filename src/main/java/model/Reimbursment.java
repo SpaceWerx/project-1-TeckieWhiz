3ff805afb06fd389f58d28383f41bf90d4502e84
christine nwonuala <chrisnwogu5@gmail.com>
2022-05-12 16:31:02 -0400

    changes

--- src/main/java/model/Reimbursment.java
+++ src/main/java/model/Reimbursment.java
@@ -4,85 +4,86 @@ public class Reimbursment
 {
 	
 	private int id;
-	private int Author;
-	private int Resolver;
-	private String Description;
+	private int author;
+	private int resolver;
+	private String description;
 	private Reimbursment_Type type;
 	private Status status;
-	private double Amount;
-		
-	public Reimbursment()
-	{
+	private double amount;
+	
+	public Reimbursment() {
 		super();
 	}
-
+	
 	public Reimbursment(int id, int author, int resolver, String description, Reimbursment_Type type, Status status,
 			double amount) {
 		super();
 		this.id = id;
-		Author = author;
-		Resolver = resolver;
-		Description = description;
+		this.author = author;
+		this.resolver = resolver;
+		this.description = description;
 		this.type = type;
 		this.status = status;
-		Amount = amount;
+		this.amount = amount;
 	}
 
 	public int getId() {
 		return id;
 	}
 
+	public void setId(int id) {
+		this.id = id;
+	}
+
 	public int getAuthor() {
-		return Author;
+		return author;
+	}
+
+	public void setAuthor(int author) {
+		this.author = author;
 	}
 
 	public int getResolver() {
-		return Resolver;
+		return resolver;
+	}
+
+	public void setResolver(int resolver) {
+		this.resolver = resolver;
 	}
 
 	public String getDescription() {
-		return Description;
+		return description;
+	}
+
+	public void setDescription(String description) {
+		this.description = description;
 	}
 
 	public Reimbursment_Type getType() {
 		return type;
 	}
 
-	public Status getStatus() {
-		return status;
-	}
-
-	public double getAmount() {
-		return Amount;
-	}
-
-	public void setId(int id) {
-		this.id = id;
-	}
-
-	public void setAuthor(int author) {
-		Author = author;
-	}
-
-	public void setResolver(int resolver) {
-		Resolver = resolver;
-	}
-
-	public void setDescription(String description) {
-		Description = description;
-	}
-
 	public void setType(Reimbursment_Type type) {
 		this.type = type;
+	}
+
+	public Status getStatus() {
+		return status;
 	}
 
 	public void setStatus(Status status) {
 		this.status = status;
 	}
 
-	public void setAmount(double amount) {
-		Amount = amount;
+	public double getAmount() {
+		return amount;
 	}
-	    
-  
+
+	public void setAmount(double amount) {
+		this.amount = amount;
+	}
+	
+	
+		
 }
+	
